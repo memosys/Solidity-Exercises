f611924fa241f4142b425343326621ce2983542a
memosys <82053242+memosys@users.noreply.github.com>
2024-02-18 13:58:32 +0530

    3

--- BasicBank/src/BasicBank.sol
+++ BasicBank/src/BasicBank.sol
@@ -2,10 +2,17 @@
 pragma solidity ^0.8.13;
 
 contract BasicBank {
+
+    uint public balance;
     /// @notice deposit ether into the contract
     /// @dev it should work properly when called multiple times
-    function addEther() external payable {}
+    function addEther() external payable {
+            balance += msg.value;
+    }
 
     /// @notice used to withdraw ether from the contract (No restriction on withdrawals)
-    function removeEther(uint256 amount) external payable {}
+    function removeEther(uint256 amount) external payable {
+        address payable to = payable(msg.sender);
+        to.transfer(amount);
+    }
 }
