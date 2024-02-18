23e15c3c06824936a315e0c6f2eb8481a8eb630f
memosys <82053242+memosys@users.noreply.github.com>
2024-02-18 15:09:21 +0530

    Update CrossContract.sol

--- CrossContract/src/CrossContract.sol
+++ CrossContract/src/CrossContract.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.13;
 
+interface ExternalOracle {
+    function price() external view returns (uint256);
+}
+
 contract CrossContract {
     /**
      * The function below is to call the price function of PriceOracle1 and PriceOracle2 contracts below and return the lower of the two prices
@@ -10,7 +14,9 @@ contract CrossContract {
         address _priceOracle1,
         address _priceOracle2
     ) external view returns (uint256) {
-        // your code here
+        uint first = ExternalOracle(_priceOracle1).price();
+        uint second = ExternalOracle(_priceOracle2).price();
+        return first < second ? first : second;
     }
 }
 
