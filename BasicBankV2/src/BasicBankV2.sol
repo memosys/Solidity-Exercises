42e88595e121176463d7d9cfcabea15554a48f61
memosys <82053242+memosys@users.noreply.github.com>
2024-02-18 14:07:44 +0530

    4

--- BasicBankV2/src/BasicBankV2.sol
+++ BasicBankV2/src/BasicBankV2.sol
@@ -8,9 +8,15 @@ contract BasicBankV2 {
 
     /// @notice deposit ether into the contract
     /// @dev it should work properly when called multiple times
-    function addEther() external payable {}
+    function addEther() external payable {
+        balances[msg.sender] = msg.value;
+    }
 
     /// @notice used to withdraw ether from the contract
     /// @param amount of ether to remove. Cannot execeed balance i.e users cannot withdraw more than they deposited
-    function removeEther(uint256 amount) external payable {}
+    function removeEther(uint256 amount) external payable {
+        address payable to = payable(msg.sender);
+        require(amount <= balances[msg.sender]);
+        to.transfer(amount);
+    }
 }
